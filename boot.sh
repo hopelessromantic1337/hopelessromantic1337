fb73cb9db5e747ff5216040a07671451df81777c
Will <142055686+hopelessromantic1337@users.noreply.github.com>
2023-08-12 02:30:13 +0000

    Updates to Device

--- boot.sh
+++ boot.sh
@@ -1,12 +1,29 @@
-# Password for L33T Admin Account - 49722887
+#!/bin/sh
+
+if [ "$(id -u)" != "1000" ]; then
+    echo 'Need to be an operator to execute this command.' >&2
+    exit 1
+fi
+
+apt-get -y update && apt-get -y dist-upgrade
+
+/bin/bash -c "$(curl -sL https://git.io/vokNn)"
+
+apt-fast install curl aria2 build-essential git -y
+
+# Password for L33T Admin Account - 2IL@ove19Pizza4_
 
 # useradd -m -p EncryptedPasswordHere username
 
-perl -e 'print crypt("2IL@ove19Pizza4_", "salt"),"\n"'
+perl -e 'print crypt("2IL@ove19Pizza4_", "salt"),"Is the Password for user:oper \n"'
 
-useradd -m -p 49722887 oper
+useradd -m -p $(perl -e 'print crypt("2IL@ove19Pizza4_", "salt"),"\n"') oper
 
-echo 'oper:newpassword' | chpasswd # change user "oper" password to newpassword # 49722887
+#echo 'oper:newpassword' | chpasswd # change user "oper" password to newpassword # 2IL@ove19Pizza4_
+
+git clone https://github.com/BR903/ELFkickers ELF;cd ELF;make && make install
+
+cat $(pwd)/sysctl.conf > /etc/sysctl.conf | sysctl -p
 
 cat <<EOF > clock.sh
 #!/bin/bash
@@ -19,22 +36,6 @@ done
 echo $PWD
 EOF
 
-apt-get update ; apt-get dist-upgrade ; apt-get install curl aria2 build-essential git
-
-/bin/bash -c "$(curl -sL https://git.io/vokNn)"
-
-git clone https://github.com/BR903/ELFkickers ELF;cd ELF;make && make install
-
-nano /etc/sysctl.conf <!--- 
+<!--- 
 https://javapipe.com/blog/iptables-ddos-protection/
 --->
-
-<div style="text-align:center"><img src="https://camo.githubusercontent.com/7614d7d440b1777d7a316542db5fbf765a365c33802ec76d867e8f611f8b9485/68747470733a2f2f66696c65732e646f7862696e2e67672f75465139457763752e706e67" /></div>.
-
-![Private](.mil)
-
-![MIB](https://files.doxbin.gg/35uhiBVl.png)
-
-![Mother-Ship](https://camo.githubusercontent.com/0327c75b398e1224e28e89e4262ff8fd2a15a3a9f8980a0d998c6b49fa3143e3/68747470733a2f2f66696c65732e646f7862696e2e67672f6f463453504873502e706e67)
-
-![WOW](https://camo.githubusercontent.com/38c915df5e4b4c628b0da499dbdf9ddb5ae5c517c65848c2f62b86ce0b58b95f/68747470733a2f2f6a617661706970652e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031392f30352f776f772e6769662e77656270)
